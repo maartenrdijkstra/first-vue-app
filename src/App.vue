e3727852d496d12e0dba976fe21d707aa97d5a3c
maartenrdijkstra <maartenrdijkstra@gmail.com>
2025-06-25 14:33:21 +0200

    opdracht 6

--- src/App.vue
+++ src/App.vue
@@ -15,10 +15,33 @@
             <button @click="toggleCompletedTask(task)">Toggle status</button>
         </li>
     </ul>
+    <p><strong>Voeg persoon toe</strong></p>
+    <label for="nieuweNaam">Naam:</label>
+    <br />
+    <input id="nieuweNaam" type="text" v-model.trim="newName" />
+    <br />
+    <label for="nieuweLeeftijd">Leeftijd:</label>
+    <br />
+    <input id="nieuweLeeftijd" type="text" v-model.number="newAge" />
+    <br />
+    <button @click="addPerson(newName, newAge)">Add Person</button>
+    <br />
+    <p>List of People:</p>
+    <ul>
+        <li v-for="(persoon, index) in people" :key="index">
+            {{ persoon }}
+        </li>
+    </ul>
+    <p>List of Children:</p>
+    <ul>
+        <li v-for="(child, index) in children" :key="index">
+            {{ child }}
+        </li>
+    </ul>
 </template>
 
 <script setup>
-import {ref, reactive} from 'vue';
+import {ref, reactive, computed} from 'vue';
 
 // Opdracht 4
 const clicks = ref(0);
@@ -53,6 +76,21 @@ const tasks = ref([
 const toggleCompletedTask = task => {
     task.completed = !task.completed;
 };
+
+//Opdracht 6 (het wordt wellicht wat onoverzichtelijk, voor de volgende opdracht gebruik ik een nieuw project :-) )
+const people = ref([
+    {name: 'Jan', age: 12},
+    {name: 'Piet', age: 20},
+]);
+
+const addPerson = function (newName, newAge) {
+    const newPerson = {name: newName, age: newAge};
+    people.value.push(newPerson);
+};
+
+const children = computed(() => {
+    return people.value.filter(persoon => persoon.age < 18);
+});
 </script>
 
 <style scoped></style>
